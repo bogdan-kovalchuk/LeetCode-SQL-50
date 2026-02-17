d8db93d3af9095b370d65d1d20bf4a186c74788a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-02-17 09:12:27 +0200

    1661 solution

--- 02 - Basic Joins/1661. Average Time of Process per Machine.sql
+++ 02 - Basic Joins/1661. Average Time of Process per Machine.sql
@@ -1,1 +1,13 @@
-# Write your MySQL query statement below
+SELECT
+  machine_id,
+  ROUND(AVG(end_ts - start_ts), 3) AS processing_time
+FROM (
+  SELECT
+    machine_id,
+    process_id,
+    MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_ts,
+    MAX(CASE WHEN activity_type = 'end'   THEN timestamp END) AS end_ts
+  FROM Activity
+  GROUP BY machine_id, process_id
+) p
+GROUP BY machine_id;
