19580220180ed2b61d18f849d1c4b454e91b9876
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-02-19 20:51:17 +0200

    1280 solution

--- 02 - Basic Joins/1280. Students and Examinations.sql
+++ 02 - Basic Joins/1280. Students and Examinations.sql
@@ -1,1 +1,14 @@
-# Write your MySQL query statement below
+SELECT
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COALESCE(COUNT(e.subject_name), 0) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+  ON e.student_id = s.student_id
+ AND e.subject_name = sub.subject_name
+GROUP BY
+    s.student_id, s.student_name, sub.subject_name
+ORDER BY
+    s.student_id, sub.subject_name;
