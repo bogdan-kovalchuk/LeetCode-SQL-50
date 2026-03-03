e5c5debc61d677f8392ff61054667ddbe446d873
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-03-03 15:46:09 +0200

    550 solution

--- 03 - Basic Aggregate Functions/550. Game Play Analysis IV.sql
+++ 03 - Basic Aggregate Functions/550. Game Play Analysis IV.sql
@@ -1,1 +1,19 @@
-# Write your MySQL query statement below
+WITH first_login AS (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_day
+    FROM Activity
+    GROUP BY player_id
+),
+returned_next_day AS (
+    SELECT DISTINCT f.player_id
+    FROM first_login f
+    JOIN Activity a
+      ON a.player_id = f.player_id
+     AND a.event_date = DATE_ADD(f.first_day, INTERVAL 1 DAY)
+)
+SELECT
+    ROUND(
+        (SELECT COUNT(*) FROM returned_next_day) / (SELECT COUNT(*) FROM first_login),
+        2
+    ) AS fraction;
