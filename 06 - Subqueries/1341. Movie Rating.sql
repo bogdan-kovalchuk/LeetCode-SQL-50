248e7edc867a807f8d8bafdc1fc3fda7709d491f
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-04-03 11:25:59 +0300

    1341 solution

--- 06 - Subqueries/1341. Movie Rating.sql
+++ 06 - Subqueries/1341. Movie Rating.sql
@@ -1,1 +1,19 @@
-# Write your MySQL query statement below
+(SELECT
+    u.name AS results
+FROM MovieRating mr
+JOIN Users u
+  ON u.user_id = mr.user_id
+GROUP BY mr.user_id, u.name
+ORDER BY COUNT(*) DESC, u.name
+LIMIT 1)
+UNION ALL
+(SELECT
+    m.title AS results
+FROM MovieRating mr
+JOIN Movies m
+  ON m.movie_id = mr.movie_id
+WHERE mr.created_at >= '2020-02-01'
+  AND mr.created_at < '2020-03-01'
+GROUP BY mr.movie_id, m.title
+ORDER BY AVG(mr.rating) DESC, m.title
+LIMIT 1);
